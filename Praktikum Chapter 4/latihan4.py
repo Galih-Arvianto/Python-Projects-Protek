86a3fa3baff7917c05f70c970eb85ee678576d20
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-10-14 22:23:37 +0800

    Update latihan4.py

--- Praktikum Chapter 4/latihan4.py
+++ Praktikum Chapter 4/latihan4.py
@@ -8,4 +8,4 @@ rest=45/60
 sampaiC=math.floor((berangkat+(jarakAB/Vab)+(JarakBC/Vbc)+rest)*60)
 jamsampaiC=math.floor(sampaiC/60)
 menitsampaic=jamsampaiC%60
-print('Pak Amir tibat ke kota c pada pukul'+str(jamsampaiC)+'lebih'+str(menitsampaic)+ 'menit')
+print('pak amir sampai jam'+str(jamsampaiC)+'lebih'+str(menitsampaic)+ 'menit')
