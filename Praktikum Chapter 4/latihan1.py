1b8b18854635644472ac5ff2ff155a411933294f
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-10-14 21:34:55 +0800

    Create latihan1.py

---
+++ Praktikum Chapter 4/latihan1.py
@@ -0,0 +1,9 @@
+tarifA=200000
+tarifB=10000
+Rentalawal=06.00
+RentalAkhir=23.50
+totalrental=RentalAkhir-Rentalawal
+waktuRentalnext=totalrental-12
+totaltarifB=waktuRentalnext * tarifB
+totalbiaya=tarifA+totaltarifB
+print (totalbiaya)
