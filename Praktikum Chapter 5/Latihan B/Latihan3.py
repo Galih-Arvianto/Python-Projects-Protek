50116b647db80e4ac37573f51642afa2d685562f
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-10-27 22:17:47 +0800

    Create Latihan3.py

---
+++ Praktikum Chapter 5/Latihan B/Latihan3.py
@@ -0,0 +1,10 @@
+i = 1
+random=0
+jumlah=0
+while (i<100):
+  print(i)
+  jumlah=jumlah+i
+  i += 2
+  random+=1
+print('Banyaknya Bilangan Ganjil : '+str(random))
+print ('Jumlah seluruh bilangan : ' +str(jumlah))
