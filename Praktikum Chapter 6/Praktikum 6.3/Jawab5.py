429717fe8733b82fd55977460373518dc4969c3b
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-10-28 22:55:35 +0800

    Update Jawab5.py

--- Praktikum Chapter 6/Praktikum 6.3/Jawab5.py
+++ Praktikum Chapter 6/Praktikum 6.3/Jawab5.py
@@ -1,50 +1,27 @@
-#A--------------------------------------------------------
-
-print ('JAWABAN A')
 from operation import *
-a=2
-b=4
-c=6
-d=4
-e=b*c
-f=e+a
-
-print(b, 'x', c, '=' ,kali(b,c))
-print(a, '+', e, '=' ,jumlah(a,e))
-print(f, '-', d, '=' ,kurang(f,d))
-
-#B--------------------------------------------------------
-
-print ('JAWABAN B')
-from operation import*
-a=4
-b=7
-c=6
-d=9
-e=a+b
-f=c-d
-
-print(a, '+', b, '=' ,jumlah(a,b))
-print(c, '-', d, '=' ,kurang(c,d))
-print(e, 'x', f, '=' ,kali(e,f))
-
-#C------------------------------------------------------------------------------------------
-print ('Jawaban C')
-from operation import*
+'''
 a=10
-b=2
-c=7
-d=5
-e=12
-f=34
-P1=a+b
-P2=c+d
-P3=e-f
-P4=P1/P2
+b=7
 
 print(a, '+', b, '=' ,jumlah(a,b))
-print(c, '+', d, '=' ,jumlah(c,d))
-print(e, '-', f, '=' ,kurang(e,f))
-print(P1, '/', P2,'=' ,bagi(P1,P2))
-print(P3, '/', P4,'=' ,bagi(P3,P4))
-
+print(a, '-', b, '=' ,kurang(a,b))
+print(a, 'x', b, '=' ,kali(a,b))
+print(a, '/', b, '=' ,bagi(a,b))
+'''
+#-------------A------------------
+A=kali(4,6)
+B=jumlah(2,A)
+C=kurang (B,4)
+print(C)
+#-------------B------------------
+D=jumlah (4,7)
+E=kurang (6,9)
+F=kali (D,E)
+print(F)
+#-------------C------------------
+H=jumlah(10,2)
+I=jumlah(7,5)
+J=kurang(12,34)
+K=bagi(H,I)
+L=bagi (K,J)
+print(L)
