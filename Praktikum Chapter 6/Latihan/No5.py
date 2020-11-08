99cede896f20122b7fff5f5add641e86fe1afda2
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-11-08 20:33:23 +0800

    Create No5.py

---
+++ Praktikum Chapter 6/Latihan/No5.py
@@ -0,0 +1,11 @@
+from statistik import *
+
+print('(5,10,3,9,30,16,2,11)')
+print('Nilai Maksimum = ', max(5,10,3,9,30,16,2,11))
+print('Nilai Minimum  = ', min(5,10,3,9,30,16,2,11))
+print('Rata-rata      = ', average(5,10,3,9,30,16,2,11))
+
+print('\n              (81,98,12,83,45,77,69,30,56)')
+print('Nilai Maks = ', max(81,98,12,83,45,77,69,30,56))
+print('Nilai Min  = ', min(81,98,12,83,45,77,69,30,56))
+print('Average      = ', average(81,98,12,83,45,77,69,30,56))
