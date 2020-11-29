ff8fc641a0cac021d119167c59704f0998698da4
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-11-30 00:44:51 +0800

    Create Project11.py

---
+++ Praktikum Chapter 8/Project11.py
@@ -0,0 +1,14 @@
+buah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6500}
+
+print('----------------------------------')
+print('---------Toko Buah-buahan---------')
+print('----------------------------------')
+
+namaBuah = list(buah.keys())
+hargaBuah = tuple(buah.values())
+data = []
+
+print('Menu')
+print('A = Tambah data buah')
+print('B = Beli buah')
+print('0 = keluar')
