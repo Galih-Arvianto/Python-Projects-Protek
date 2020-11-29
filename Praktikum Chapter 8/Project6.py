aaa24276a38a804d6cf9370476c34ed98578dacc
Galih-Arvianto N <72035766+Galih-Arvianto@users.noreply.github.com>
2020-11-30 00:33:53 +0800

    Create Project6.py

---
+++ Praktikum Chapter 8/Project6.py
@@ -0,0 +1,7 @@
+def sortStringByChar():
+    Data = ['apel', 'rambutan', 'jeruk ']
+    for data in range(len(Data)):
+        Data.sort(reverse = True)
+        return Data
+
+print(sortStringByChar())
